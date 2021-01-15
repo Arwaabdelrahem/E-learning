18cc0f3b66f526afa9e004b490ea8a7f79449f26
ArwaAbdelrahem <arwaabdelrahem22@gmail.com>
2021-01-15 23:42:25 +0200

    DB connection

--- startup/DB.js
+++ startup/DB.js
@@ -3,15 +3,12 @@ const config = require("config");
 
 module.exports = function () {
   mongoose
-    .connect(
-      "mongodb+srv://Arwaabdelrahem:mongo@cluster0.xse5n.mongodb.net/elearning?retryWrites=true&w=majority",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-      }
-    )
+    .connect(config.get("DB"), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    })
     .then(() => {
       console.log("MongoDB connected");
     });
