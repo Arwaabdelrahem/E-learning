c44a3741a22d1bb836c9a012d6c481779311db8a
ArwaAbdelrahem <arwaabdelrahem22@gmail.com>
2021-01-17 18:58:43 +0200

    handeling index routes

--- index.js
+++ index.js
@@ -1,35 +1,9 @@
 const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const admin = require("./routes/Admin");
-const user = require("./routes/Users");
-const teacher = require("./routes/Teachers");
-const material = require("./routes/Materials");
-const timeline = require("./routes/Timeline");
-const student = require("./routes/Students");
-const question = require("./routes/Questions");
-const { errorHandler, serverErrorHandler } = require("./middleware/error");
-const exam = require("./routes/Exams");
-
 const app = express();
 
 require("./startup/DB")();
-app.use(cors());
-app.use("Uploads", express.static("Uploads"));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+require("./startup/routes")(app);
 require("./startup/production")(app);
-
-app.use("/admin", admin);
-app.use("/users", user);
-app.use("/teachers", teacher);
-app.use("/students", student);
-app.use("/teachers/material", material);
-app.use("/questions", question);
-app.user("/exams", exam);
-app.use("/timeline", timeline);
-app.use(errorHandler);
-app.use(serverErrorHandler);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
---
+++ startup/routes.js
@@ -0,0 +1,29 @@
+const cors = require("cors");
+const express = require("express");
+const bodyParser = require("body-parser");
+const admin = require("../routes/Admin");
+const user = require("../routes/Users");
+const teacher = require("../routes/Teachers");
+const material = require("../routes/Materials");
+const timeline = require("../routes/Timeline");
+const student = require("../routes/Students");
+const question = require("../routes/Questions");
+const exam = require("../routes/Exams");
+const { errorHandler, serverErrorHandler } = require("../middleware/error");
+
+module.exports = function (app) {
+  app.use(cors());
+  app.use("Uploads", express.static("Uploads"));
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+  app.use("/admin", admin);
+  app.use("/users", user);
+  app.use("/teachers", teacher);
+  app.use("/students", student);
+  app.use("/teachers/material", material);
+  app.use("/questions", question);
+  app.use("/exams", exam);
+  app.use("/timeline", timeline);
+  app.use(errorHandler);
+  app.use(serverErrorHandler);
+};
