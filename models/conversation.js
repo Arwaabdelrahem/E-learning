e2e168edf0b322a4d4b4ec130276f94f1b9e7212
ArwaAbdelrahem <arwaabdelrahem22@gmail.com>
2021-04-24 01:13:23 +0200

    updating conversation model

--- models/conversation.js
+++ models/conversation.js
@@ -1,34 +1,65 @@
+const Joi = require("joi");
+const { join } = require("lodash");
 const mongoose = require("mongoose");
 const mongooseAutoIncrement = require("mongoose-auto-increment");
 const pagination = require("mongoose-paginate-v2");
 
 mongooseAutoIncrement.initialize(mongoose.connection);
 
-const schema = new mongoose.Schema(
+const conversationSchema = new mongoose.Schema(
   {
+    name: {
+      type: String, // in case of more than 2 users
+    },
+    image: {
+      type: String, // link to image in same case as name
+    },
     users: [
       {
         type: Number,
         ref: "User",
       },
     ],
-    meta: [metaSchema()],
+    owner: {
+      type: Number,
+      ref: "User",
+    },
+    conversationType: {
+      type: String,
+      enum: ["P2P", "GROUP"],
+      required: true,
+    },
+    course: {
+      type: Number,
+      ref: "course",
+    },
     lastMessage: {
       type: Number,
-      ref: "Message",
+      ref: "message",
     },
+    meta: [metaSchema()],
   },
   { timestamps: true }
 );
+
+conversationSchema.methods.containUser = function (userID) {
+  for (let user of this.users) if (user._id == userID) return true;
+  return false;
+};
 
 schema.set("toJSON", {
   virtuals: true,
   transform: function (doc) {
     return {
       id: doc.id,
+      conversationType: doc.conversationType,
+      name: doc.name,
+      owner: doc.owner,
       users: doc.users,
+      course: doc.course,
       meta: doc.meta,
       lastMessage: doc.lastMessage,
+      image: doc.image,
       createdAt: doc.createdAt,
       updatedAt: doc.updatedAt,
     };
@@ -40,7 +71,7 @@ function metaSchema() {
     {
       user: {
         type: Number,
-        ref: "User",
+        ref: "user",
       },
       countOfUnseenMessages: {
         type: Number,
@@ -62,6 +93,24 @@ function metaSchema() {
   return schema;
 }
 
+function PRIVATE(conversation) {
+  const schema = Joi.object({
+    name: Joi.forbidden(),
+    image: Joi.forbidden(),
+    uesrs: Joi.array().unique().min(2).required(),
+  });
+  return schema.validate(conversation);
+}
+
+function GROUP(conversation) {
+  const schema = Joi.object({
+    name: Joi.string().required(),
+    image: Joi.string(),
+    uesrs: Joi.array().unique().min(2).required(),
+  });
+  return schema.validate(conversation);
+}
+
 schema.plugin(pagination);
 schema.plugin(mongooseAutoIncrement.plugin, {
   model: "Conversation",
@@ -70,3 +119,5 @@ schema.plugin(mongooseAutoIncrement.plugin, {
 
 const Conversation = mongoose.model("Conversation", schema);
 module.exports.Conversation = Conversation;
+module.exports.PRIVATE = PRIVATE;
+module.exports.GROUP = GROUP;
